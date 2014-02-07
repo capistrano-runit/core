ebf3241ddac9bb53511778905193fabfa9b75296
Anton Ageev <antage@gmail.com>
2014-02-07 10:56:49 +0400

    Fix error: ruby can't 'require' rake files.

--- lib/capistrano/puma.rb
+++ lib/capistrano/puma.rb
@@ -1,3 +1,3 @@
 Dir.glob(File.expand_path("../puma/*.rake", __FILE__)) do |file|
-  require file
+  load file
 end
--- lib/capistrano/runit.rb
+++ lib/capistrano/runit.rb
@@ -1,3 +1,3 @@
 Dir.glob(File.expand_path("../runit/*.rake", __FILE__)) do |file|
-  require file
+  load file
 end
